6e80e5248a7321bbd204419cbf459ddaf7da5f6c
satriaranggaj <satriarangga43@gmail.com>
2026-06-01 01:35:56 +0700

    refactor: Simplify user authentication data structure in shared props

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -38,12 +38,8 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
-           'auth' => [
-                    'user' => $request->user() ? [
-                    'id' => $request->user()->id,
-                    'name' => $request->user()->name,
-                    'email' => $request->user()->email,
-                ] : null,
+            'auth' => [
+                'user' => $request->user(),
             ],
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
         ];
